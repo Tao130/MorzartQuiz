1db3182d41b50013ade2032ba9c5356381bbddcb
watanabe <momossy5>
2020-03-03 13:37:30 +0900

    MediaPlayer instanceを作成 res/rawを追加

--- app/src/main/java/com/example/morzartquiz/GameFragment.kt
+++ app/src/main/java/com/example/morzartquiz/GameFragment.kt
@@ -1,24 +1,40 @@
 package com.example.morzartquiz
 
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.morzartquiz.databinding.FragmentGameBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class GameFragment : Fragment() {
 
+    private lateinit var player: MediaPlayer
+    private lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
-    }
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_game, container, false)
+
+        player = MediaPlayer.create(this.context, Uri.parse("android.resource://getPackageName()/raw/sonata_no25"))
+
+        //再生ボタンとメディアプレイヤーの紐付け
+        binding.startButton.setOnClickListener {
+            player.start()
+        }
+        return binding.root
+        }
+
 
 
 }
